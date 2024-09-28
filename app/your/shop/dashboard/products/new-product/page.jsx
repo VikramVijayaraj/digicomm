bcd06d405ea1f2a1529c30df3684d4a50de2f20a
Vikram Vijayaraj <vikramvijayaraj@Vikrams-MacBook-Pro.local>
2024-09-28 12:08:28 +0530

    Fix bug new-product.jsx

--- app/your/shop/dashboard/products/new-product/page.jsx
+++ app/your/shop/dashboard/products/new-product/page.jsx
@@ -1,7 +1,10 @@
 import { getCategories } from "@/lib/db/categories";
 import NewProductForm from "@/components/product/new-product/new-product-form";
+import { auth } from "@/auth";
 
 export default async function NewProductPage() {
+  const session = await auth();
+
   const categories = await getCategories();
 
   return (
