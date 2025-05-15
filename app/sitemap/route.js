a42701e9ecb63cd5d2aeb248d8c7e38a1b4c813f
Vikram Vijayaraj <vikramvijayaraj@Vikrams-MacBook-Pro.local>
2025-05-15 10:25:54 +0530

    Add category paths in sitemap

--- app/sitemap/route.js
+++ app/sitemap/route.js
@@ -2,17 +2,24 @@ import { revalidatePath } from "next/cache";
 
 import { getActiveBlogPosts } from "@/lib/db/blog";
 import { getProducts } from "@/lib/db/products";
+import { getCategories } from "@/lib/db/categories";
 
 export async function GET() {
   try {
     revalidatePath("/sitemap");
 
     const products = await getProducts();
+    const categories = await getCategories();
     const blogPosts = await getActiveBlogPosts();
 
     const productPaths = products.map((product) => ({
       url: `${process.env.NEXT_PUBLIC_APP_BASE_URL}/products/${product.product_slug}`,
       lastModified: new Date(product.updated_at).toISOString(),
+    }));
+
+    const categoryPaths = categories.map((category) => ({
+      url: `${process.env.NEXT_PUBLIC_APP_BASE_URL}/categories/${category.slug}`,
+      lastModified: new Date(category.updated_at).toISOString(),
     }));
 
     const blogPostsPaths = blogPosts.map((post) => ({
@@ -54,7 +61,7 @@ export async function GET() {
     // Generate sitemap XML
     const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${[...productPaths, ...blogPostsPaths, ...staticPaths]
+      ${[...productPaths, ...categoryPaths, ...blogPostsPaths, ...staticPaths]
         .map(
           (path) => `
         <url>
