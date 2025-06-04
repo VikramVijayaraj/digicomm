5eb71bceceff4cf1fa149c4aa9b1f56600a6023f
Vikram Vijayaraj <vikramvijayaraj@Vikrams-MacBook-Pro.local>
2025-06-04 22:21:29 +0530

    Banner CTA design fix

--- components/cta/banner-cta.jsx
+++ components/cta/banner-cta.jsx
@@ -4,9 +4,12 @@ export default function BannerCTA() {
   return (
     <div
       className="bg-[url(https://images.unsplash.com/photo-1698807390276-725f3a7e41cf?q=80&w=3432&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)]
-        h-[500px] bg-cover"
+        h-[400px] lg:h-[500px] bg-cover"
     >
-      <div className="flex flex-col items-center justify-center h-full bg-black/50">
+      <div
+        className="global-padding text-center flex flex-col items-center justify-center h-full
+          bg-black/50"
+      >
         <h2 className="text-3xl md:text-5xl font-bold text-white mb-4">
           Launch Your Digital Store Today
         </h2>
