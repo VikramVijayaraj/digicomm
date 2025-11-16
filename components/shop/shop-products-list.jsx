76b537382221c67805785e0f4f9be17fbf44d1eb
Vikram Vijayaraj <vikramvijayaraj31@gmail.com>
2025-11-16 11:48:06 +0530

    Update: shop products table to cards list

--- components/shop/shop-products-list.jsx
+++ components/shop/shop-products-list.jsx
@@ -3,6 +3,14 @@ import { IndianRupee } from "lucide-react";
 import { getShopProducts } from "@/lib/db/sellers";
 import { dateConverter } from "@/utils/dateConverter";
 import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
   Table,
   TableBody,
   TableCell,
@@ -12,6 +20,8 @@ import {
 } from "@/components/ui/table";
 import ShopProductActions from "./shop-product-actions";
 import { createClient } from "@/utils/supabase/server";
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function ShopProductsList() {
   const supabase = await createClient();
@@ -24,38 +34,46 @@ export default async function ShopProductsList() {
   }
 
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Product</TableHead>
-          <TableHead>Category</TableHead>
-          {/* <TableHead>Stock</TableHead> */}
-          <TableHead>Price</TableHead>
-          <TableHead>Modified At</TableHead>
-          <TableHead className="w-4"></TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody className="hover:bg-none">
-        {products.map((product) => (
-          <TableRow key={product.product_id}>
-            <TableCell>{product.product_name}</TableCell>
-            <TableCell className="truncate">{product.category_name}</TableCell>
-            {/* <TableCell>{product.stock}</TableCell> */}
-            <TableCell>
-              <p className="flex items-center">
-                <IndianRupee size={15} />
-                {product.price}
-              </p>
-            </TableCell>
-            <TableCell>{dateConverter(product.updated_at)}</TableCell>
-
-            {/* Actions */}
-            <TableCell>
-              <ShopProductActions product={product} />
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <div className="space-y-4">
+      {products.map((product) => (
+        <Card key={product.product_id}>
+          <CardHeader>
+            <div className="flex justify-between items-start gap-4">
+              <div className="flex items-start gap-4">
+                <Image
+                  src={product.images.length > 0 && product.images[0]}
+                  width="100"
+                  height="100"
+                  className="rounded-lg object-cover"
+                  alt="Product Image"
+                />
+                <div>
+                  <CardTitle className="text-lg">
+                    <Link
+                      href={`/products/${product.product_slug}`}
+                      className="hover:underline"
+                    >
+                      {product.product_name}
+                    </Link>
+                  </CardTitle>
+                  <CardDescription>{product.category_name}</CardDescription>
+                </div>
+              </div>
+              <div>
+                <ShopProductActions product={product} />
+              </div>
+            </div>
+          </CardHeader>
+          <CardContent className="flex justify-between items-center">
+            <p className="text-gray-500">{dateConverter(product.updated_at)}</p>
+            <p className="flex items-center text-green-600 font-semibold">
+              <IndianRupee size={15} />
+              {product.price}
+            </p>
+          </CardContent>
+          {/* <CardFooter></CardFooter> */}
+        </Card>
+      ))}
+    </div>
   );
 }
